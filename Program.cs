02856f8319bdf20f628c743fea9b572097a3f95a
Cherkesky <guy@cherkesky.com>
2020-05-11 15:55:25 -0500

    added 2 more virtual methods

--- Program.cs
+++ Program.cs
@@ -17,6 +17,14 @@ namespace colors_sounds
     {
       Console.WriteLine("Vrooom!");
     }
+    public virtual void Stop()
+    {
+      System.Console.WriteLine("Stopping!");
+    }
+    public virtual void Turn()
+    {
+      System.Console.WriteLine("Stopping!");
+    }
   }
 
   public class Zero : Vehicle
@@ -27,6 +35,14 @@ namespace colors_sounds
     {
       System.Console.WriteLine("Brrrrrrrrr!!!!");
     }
+     public override void Stop()
+    {
+      System.Console.WriteLine("Zero is stopping!");
+    }
+     public override void Turn()
+    {
+      System.Console.WriteLine("Zero is turning!");
+    }
   }
 
   public class Cessna : Vehicle
@@ -36,42 +52,76 @@ namespace colors_sounds
     {
       Console.WriteLine("Zoooooom!");
     }
+      public override void Stop()
+    {
+      System.Console.WriteLine("Cessna is stopping!");
+    }
+     public override void Turn()
+    {
+      System.Console.WriteLine("Cessna is turning!");
+    }
   }
 
 
-public class Tesla : Vehicle
-{  // Electric car
-  public double BatteryKWh { get; set; }
-  public void ChargeBattery() { System.Console.WriteLine("Charged"); }
-  public override void Drive()
-  {
-    System.Console.WriteLine("Zooooooooink!");
+  public class Tesla : Vehicle
+  {  // Electric car
+    public double BatteryKWh { get; set; }
+    public void ChargeBattery() { System.Console.WriteLine("Charged"); }
+    public override void Drive()
+    {
+      System.Console.WriteLine("Zooooooooink!");
+    }
+      public override void Stop()
+    {
+      System.Console.WriteLine("Tesla is stopping!");
+    }
+     public override void Turn()
+    {
+      System.Console.WriteLine("Tesla is turning!");
+    }
   }
-}
-public class Ram : Vehicle
-{  // Gas powered truck
-  public double FuelCapacity { get; set; }
-  public void RefuelTank() { System.Console.WriteLine("Fueled"); }
-  public override void Drive()
-  {
-    System.Console.WriteLine("Chakachakachakachaka!");
+  public class Ram : Vehicle
+  {  // Gas powered truck
+    public double FuelCapacity { get; set; }
+    public void RefuelTank() { System.Console.WriteLine("Fueled"); }
+    public override void Drive()
+    {
+      System.Console.WriteLine("Chakachakachakachaka!");
+    }
+      public override void Stop()
+    {
+      System.Console.WriteLine("Ram is stopping!");
+    }
+     public override void Turn()
+    {
+      System.Console.WriteLine("Ram is turning!");
+    }
   }
-}
 
 
-class Program
-{
-  static void Main(string[] args)
+  class Program
   {
-      Zero motorcycle = new Zero(){MainColor="Black", MaximumOccupancy="1"};
-      Cessna cess = new Cessna(){MainColor="White", MaximumOccupancy="4"};
-      Tesla musk = new Tesla(){MainColor="Blue", MaximumOccupancy="5"};
-      Ram dixie = new Ram(){MainColor="Red", MaximumOccupancy="6"};
+    static void Main(string[] args)
+    {
+      Zero motorcycle = new Zero() { MainColor = "Black", MaximumOccupancy = "1" };
+      Cessna cess = new Cessna() { MainColor = "White", MaximumOccupancy = "4" };
+      Tesla musk = new Tesla() { MainColor = "Blue", MaximumOccupancy = "5" };
+      Ram dixie = new Ram() { MainColor = "Red", MaximumOccupancy = "6" };
 
       motorcycle.Drive();
       cess.Drive();
       musk.Drive();
       dixie.Drive();
+
+      motorcycle.Stop();
+      cess.Stop();
+      musk.Stop();
+      dixie.Stop();
+
+      motorcycle.Turn();
+      cess.Turn();
+      musk.Turn();
+      dixie.Turn();
+    }
   }
-}
 }
