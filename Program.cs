a0240499d30c781a937de51bd2a265fa31285993
Cherkesky <guy@cherkesky.com>
2020-05-11 15:42:34 -0500

    overriding the virtual parent fn

--- Program.cs
+++ Program.cs
@@ -11,39 +11,61 @@ namespace colors_sounds
   {
     public string MainColor { get; set; }
     public string MaximumOccupancy { get; set; }
+    // VIRTUAL FN THAT CAN BE OVERRIDEN BY A DERIVED CLASS
+    public virtual void Drive()
+    {
+      Console.WriteLine("Vrooom!");
+    }
   }
 
   public class Zero : Vehicle
   {  // Electric motorcycle
     public double BatteryKWh { get; set; }
     public void ChargeBattery() { System.Console.WriteLine("Charged"); }
+    public override void Drive()
+    {
+      System.Console.WriteLine("Brrrrrrrrr!!!!");
+    }
   }
 
   public class Cessna : Vehicle
   {  // Propellor light aircraft
     public string MaximumOccupancy { get; set; }
     public void RefuelTank() { System.Console.WriteLine("Fueled"); }
-  }
-
-  public class Tesla : Vehicle
-  {  // Electric car
-    public double BatteryKWh { get; set; }
-    public void ChargeBattery() { System.Console.WriteLine("Charged"); }
-  }
-  public class Ram : Vehicle
-  {  // Gas powered truck
-    public double FuelCapacity { get; set; }
-    public void RefuelTank() { System.Console.WriteLine("Fueled"); }
-  }
-
-
-
-
-  class Program
-  {
-    static void Main(string[] args)
+    public override void Drive()
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Zoooooom!");
     }
   }
+}
+
+public class Tesla : Vehicle
+{  // Electric car
+  public double BatteryKWh { get; set; }
+  public void ChargeBattery() { System.Console.WriteLine("Charged"); }
+  public override void Drive()
+  {
+    System.Console.WriteLine("Zooooooooink!");
+  }
+}
+public class Ram : Vehicle
+{  // Gas powered truck
+  public double FuelCapacity { get; set; }
+  public void RefuelTank() { System.Console.WriteLine("Fueled"); }
+  public override void Drive()
+  {
+    System.Console.WriteLine("Chakachakachakachaka!");
+  }
+}
+
+
+
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World!");
+  }
+}
 }
