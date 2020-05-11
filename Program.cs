722a5f1c193a837af670e46f8ff9142f212ac501
Cherkesky <guy@cherkesky.com>
2020-05-11 15:38:24 -0500

    inheritance from Vehicle parent class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿// Move all common properties in your vehicles to a new Vehicle class.
+// Create an instance of each vehicle.
+// Define a different value for each vehicle's properties.
+// Create a Drive() method in the Vehicle class.
+// Override the Drive() method in all the other vehicle classes. Include the vehicle's color in the message (i.e. "The blue Ram drives past. RRrrrrrummbbble!").
+// using System;
 
 namespace colors_sounds
 {
-    class Program
+  public class Vehicle
+  {
+    public string MainColor { get; set; }
+    public string MaximumOccupancy { get; set; }
+  }
+
+  public class Zero : Vehicle
+  {  // Electric motorcycle
+    public double BatteryKWh { get; set; }
+    public void ChargeBattery() { System.Console.WriteLine("Charged"); }
+  }
+
+  public class Cessna : Vehicle
+  {  // Propellor light aircraft
+    public string MaximumOccupancy { get; set; }
+    public void RefuelTank() { System.Console.WriteLine("Fueled"); }
+  }
+
+  public class Tesla : Vehicle
+  {  // Electric car
+    public double BatteryKWh { get; set; }
+    public void ChargeBattery() { System.Console.WriteLine("Charged"); }
+  }
+  public class Ram : Vehicle
+  {  // Gas powered truck
+    public double FuelCapacity { get; set; }
+    public void RefuelTank() { System.Console.WriteLine("Fueled"); }
+  }
+
+
+
+
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
     }
+  }
 }
